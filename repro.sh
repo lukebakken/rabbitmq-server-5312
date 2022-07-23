12faea8faecd99299287fb5f833fc16bbd73818c
Luke Bakken <luke@bakken.io>
2022-07-23 16:15:44 -0700

    Set classic queue to use v2

--- repro.sh
+++ repro.sh
@@ -56,7 +56,7 @@ make -C "$dir/rabbitmq-perf-test" \
 sleep 10
 
 make -C "$dir/rabbitmq-perf-test" \
-    ARGS="--queue input --uri amqp://localhost:5672 --auto-delete false --flag persistent --queue-args x-dead-letter-exchange=,x-dead-letter-routing-key=dlq --producers 0 --consumers 10 --nack --requeue false --cmessages $msg_count" \
+    ARGS="--queue input --uri amqp://localhost:5672 --auto-delete false --flag persistent --queue-args x-dead-letter-exchange=,x-dead-letter-routing-key=dlq --producers 0 --consumers 12 --nack --requeue false --cmessages $((msg_count / 3))" \
     run &
 
 wait
