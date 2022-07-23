1d9c0cbe7bcd6afce8154257363ed629fe6bf5bf
Luke Bakken <luke@bakken.io>
2022-07-23 16:10:46 -0700

    Make `dlq` a quorum queue

--- repro.sh
+++ repro.sh
@@ -45,7 +45,7 @@ make -C "$dir/rabbitmq-server" TEST_TMPDIR="$test_tmpdir" RABBITMQ_CONFIG_FILE="
 
 "$rabbitmqctl_bin" --node rabbit-1 set_policy --apply-to queues ha '.' '{"ha-mode":"all", "ha-sync-mode": "automatic", "queue-mode": "lazy"}'
 
-"$rabbitmqadmin_bin" declare queue name=dlq auto_delete=false durable=true
+"$rabbitmqadmin_bin" declare queue name=dlq auto_delete=false durable=true queue_type=quorum
 
 declare -ri msg_count=250000
 
@@ -56,7 +56,7 @@ make -C "$dir/rabbitmq-perf-test" \
 sleep 10
 
 make -C "$dir/rabbitmq-perf-test" \
-    ARGS="--queue input --uri amqp://localhost:5672 --auto-delete false --flag persistent --queue-args x-dead-letter-exchange=,x-dead-letter-routing-key=dlq --producers 0 --consumers 10 --nack --requeue false --cmessages $msg_count" \
+    ARGS="--queue input --uri amqp://localhost:5672 --auto-delete false --flag persistent --queue-args x-dead-letter-exchange=,x-dead-letter-routing-key=dlq --producers 0 --consumers 8 --nack --requeue false --cmessages $((msg_count / 2))" \
     run &
 
 wait
